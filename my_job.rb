945c20ef0543b9fc8b0a18c3b950c81fa6b4e0c8
JavierARivera <116836558+JavierARivera@users.noreply.github.com>
2025-11-05 12:56:26 -0500

    Update my_job.rb

--- my_job.rb
+++ my_job.rb
@@ -1,10 +1,15 @@
 require 'date'
 
 now = DateTime.now.new_offset(0)
-puts "Hello from Github Actions a #{now} UTC"
+puts "Hey Jav! From Github Actions a #{now} UTC"
 
-if ENV['MY_API_KEY']
-  puts "API_KEY present? true"
-else
-  puts "API_KEY present? false"
-end 
+movies = [
+  { title: "Hereditary", rating: 8.1 },
+  { title: "The Witch", rating: 7.0 },
+  { title: "The Babadook", rating: 6.9 },
+  { title: "Get Out", rating: 7.8 }
+]
+
+movies.each do |movie|
+  puts "#{movie[:title]} -> #{movie[:rating]} -> #{now}"
+end
